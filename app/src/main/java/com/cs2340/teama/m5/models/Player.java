4d481c6050cc0317de98e4d3e077a8ada65eed86
Animesh Agrawal <animesh.a.777@gmail.com>
2019-02-17 13:17:20 -0500

    Wrote Player class and created and wrote SkillType class.

--- app/src/main/java/com/cs2340/teama/m5/models/Player.java
+++ app/src/main/java/com/cs2340/teama/m5/models/Player.java
@@ -1,4 +1,43 @@
 package com.cs2340.teama.m5.models;
 
 public class Player {
+    private String name;
+    private SkillType[] skills;
+    private ShipType ship;
+    private int credits;
+
+    public Player(String name) {
+        this.name = name;
+        this.ship = ShipType.GNAT;
+        skills = SkillType.values();
+        credits = 1000;
+    }
+
+    public void incrementSkill(SkillType skill, int increase) {
+        for(SkillType sk: skills) {
+            if(sk == skill)  {
+                sk.incrementSkillPointBy(increase);
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SkillType[] getSkills() {
+        return skills;
+    }
+
+    public ShipType getShip() {
+        return ship;
+    }
+
+    public void setCredits(int newCreditValue) {
+        this.credits = newCreditValue;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
 }
---
+++ app/src/main/java/com/cs2340/teama/m5/models/SkillType.java
@@ -0,0 +1,19 @@
+package com.cs2340.teama.m5.models;
+
+public enum SkillType {
+    PILOT(0), FIGHTER(0), TRADER(0), ENGINEER(0);
+
+    private int skillPointsAllocated;
+
+    SkillType(int initSkillPts) {
+        skillPointsAllocated = initSkillPts;
+    }
+
+    public int getSkillPointsAllocated() {
+        return skillPointsAllocated;
+    }
+
+    public void incrementSkillPointBy(int increase) {
+        this.skillPointsAllocated += increase;
+    }
+}
