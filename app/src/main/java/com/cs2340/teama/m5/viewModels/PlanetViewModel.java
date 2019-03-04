c0757cb8407bed48ff0147e69768514bea5a7bfb
Ed Nava <navaem@gmail.com>
2019-03-04 11:05:49 -0500

    Added Planet activity and Game

---
+++ app/src/main/java/com/cs2340/teama/m5/viewModels/PlanetViewModel.java
@@ -0,0 +1,11 @@
+package com.cs2340.teama.m5.viewModels;
+
+import android.arch.lifecycle.ViewModel;
+
+public class PlanetViewModel extends ViewModel {
+
+
+    public CharSequence getPlanetName() {
+        return "earth";
+    }
+}
---
+++ app/src/main/java/com/cs2340/teama/m5/views/PlanetActivity.java
@@ -0,0 +1,33 @@
+package com.cs2340.teama.m5.views;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cs2340.teama.m5.viewModels.PlanetViewModel;
+
+public class PlanetActivity extends AppCompatActivity {
+
+    private PlanetViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planet);
+        viewModel = ViewModelProviders.of(this).get(PlanetViewModel.class);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        TextView planet_name = (TextView) findViewById(R.id.planet_name);
+        planet_name.setText(viewModel.getPlanetName());
+
+    }
+
+}
