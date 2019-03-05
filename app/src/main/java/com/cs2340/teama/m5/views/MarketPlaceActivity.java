1c40b2c31a351fad1d5918c0fa250db621d27035
Ed Nava <navaem@gmail.com>
2019-03-04 22:17:55 -0500

    Created a RecyclerView for the MarketPlace

--- app/src/main/java/com/cs2340/teama/m5/views/MarketPlaceActivity.java
+++ app/src/main/java/com/cs2340/teama/m5/views/MarketPlaceActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -11,15 +13,28 @@ import com.cs2340.teama.m5.R;
 
 public class MarketPlaceActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_place);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        recyclerView = (RecyclerView) findViewById(R.id.market_recycler_view);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
 
     }
 
