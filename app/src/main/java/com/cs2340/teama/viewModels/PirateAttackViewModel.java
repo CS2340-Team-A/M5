2bafdcb5a854ae4b08cb053fb39cd9649c2f5512
Daniel Hsing <hsing.daniel@gmail.com>
2019-04-04 20:24:04 -0400

    feat: persist pirate attacks
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- app/src/main/java/com/cs2340/teama/viewModels/PirateAttackViewModel.java
+++ app/src/main/java/com/cs2340/teama/viewModels/PirateAttackViewModel.java
@@ -5,17 +5,32 @@ import android.arch.lifecycle.ViewModel;
 import com.cs2340.teama.models.Game;
 import com.cs2340.teama.models.Player;
 import com.cs2340.teama.models.TradeGood;
+import com.cs2340.teama.models.realm.PlayerModel;
 
 import java.util.List;
+
+import io.realm.Realm;
 
 public class PirateAttackViewModel extends ViewModel {
 
     public void pillage() {
         List<TradeGood> goods = getPlayer().getShip().getCargoHold();
-        for (TradeGood curGood : goods) {
+        for (final TradeGood curGood : goods) {
             if (getPlayer().canSell(curGood)) {
                 getPlayer().getShip().removeFromCargoHold(new TradeGood(0,
                         curGood.getGoodType(), 1));
+                Realm realm = Realm.getDefaultInstance();
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        PlayerModel playerModel = realm.where(PlayerModel.class).findFirst();
+                        if (playerModel != null) {
+                            playerModel.getShip().removeFromCargoHold(
+                                    curGood.getGoodType().toString(), 1
+                            );
+                        }
+                    }
+                });
             }
         }
     }
